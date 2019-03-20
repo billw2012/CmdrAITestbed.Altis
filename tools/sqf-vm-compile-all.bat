db242f110057a57e189924e873e138ee92512a0d
billw2012 <billw2011@gmail.com>
2019-03-20 13:48:54 +0000

    Adding compile all task.

---
+++ tools/sqf-vm-compile-all.bat
@@ -0,0 +1,10 @@
+@echo off 
+cd /d "%~dp0SQF-VM"
+
+FOR /R "%~dp0.." %%G in (*.sqf) DO (
+    echo sqfvm.exe -a --parse-only --load "./../.." -i "%%G"
+    sqfvm.exe -a --parse-only --load "./../.." -i "%%G"
+)
+
+rem sqfvm.exe -a --parse-only --load "./../../Project_0.Altis" -i "./../../%~1"
+rem sqfvm.exe -a --load "./../../Project_0.Altis" -i "./../../%~1"
